43a52a6b78e942bd82ea834a2410e83141b28d71
LiuKeCode <LiuKeCode@hotmail.com>
2022-06-29 18:16:50 +0800

    add venue_list

---
+++ apifiny/lib/venue_list.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+====================================
+@File    :  venue.py
+@Time    :  2022/06/28 14:17:01
+@Author  :  LiuKeCode@hotmail.com
+@Desc    :  None
+====================================
+"""
+
+# here put the import lib
+
+HEX = "GBBO"
+BINANCE = "BINANCE"
+BINANCEUS = "BINANCEUS"
+COINBASEPRO = "COINBASEPRO"
+FTX = "FTX"
+HUOBI = "HUOBI"
+KUCOIN = "KUCOIN"
+OKCOIN = "OKCOIN"
+OKX = "OKX"
+
+BITBAY = "BITBAY"
+BITHUMB = "BITHUMB"
+BITMAX = "BITMAX"
+BITSO = "BITSO"
+BITSTAMP = "BITSTAMP"
+BITTREX = "BITTREX"
+BLOCKCHAIN = "BLOCKCHAIN"
+COINBASEPROUK = "COINBASEPROUK"
+COINONE = "COINONE"
+CRYPTO = "CRYPTO"
+EXMO = "EXMO"
+GATEIO = "GATEIO"
+GEMINI = "GEMINI"
+INDEPENDENTRESERVE = "INDEPENDENTRESERVE"
+INDODAX = "INDODAX"
+ITBIT = "ITBIT"
+KORBIT = "KORBIT"
+KRAKEN = "KRAKEN"
+KRAKENUK = "KRAKENUK"
+LIQUID = "LIQUID"
+UPBIT = "UPBIT"
